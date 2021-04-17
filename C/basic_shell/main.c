d9f4af12f3f1971e40e82c2e9a428d172d04b677
timetravel_0 <timetravel_1010@protonmail.com>
2021-04-17 13:25:36 -0500

    Shell v1, parece que funciona.

--- C/basic_shell/main.c
+++ C/basic_shell/main.c
@@ -2,16 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include <stdbool.h>
 #include "utils.h"
+
+#define SIZE 255
 
 int main(void)
 {
-    char* argv[256];
+    char* argv[SIZE];
+    int status;
+    pid_t pid;
 
+    bzero(argv, sizeof(char)*SIZE); /* Para llenar de 0 el vector argv. */
     printf(":| &> ");
-    read_command(argv);
-    
-    execvp(argv[0], argv);
-    
+
+    do
+    {
+        read_command(argv);
+
+        if (!strcmp(argv[0], "exit")) return 0;
+
+        pid = fork();
+        assert(pid >= 0);
+
+        if (pid == 0)
+        { /* El proceso hijo trata de ejecutar el comando */
+            execvp(argv[0], argv);
+
+            perror("Return from execvp() not expected.");
+            return EXIT_FAILURE;
+        }
+
+        /* El proceso padre espera y recibe lo retornado por el hijo */
+        wait(&status);
+        if (WIFEXITED(status)) /* Se verifica que el estado es de salida. */
+        {
+            if (WEXITSTATUS(status) == 0)
+                printf(":) &> ");
+            else
+                printf(":( &> ");
+        }
+        else
+            return 1;
+
+    } while (true);
+
     return 0;
 }
